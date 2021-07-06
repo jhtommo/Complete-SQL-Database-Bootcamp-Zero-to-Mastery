104abe475b5f653e53641d1758c555954231f05d
jhtommo <63649120+jhtommo@users.noreply.github.com>
2021-07-06 03:47:22 +0100

    Update questions.sql

--- SQL Deep Dive/BETWEEN + AND/questions.sql
+++ SQL Deep Dive/BETWEEN + AND/questions.sql
@@ -2,10 +2,12 @@
 -- (include 30 and 50 in the results)
 
 /*
-* Write your query here
+* SELECT firstname, lastname, age, income
+WHERE age BETWEEN 30 AND 50 AND income < 50000;
 */
 
 -- What is the average income between the ages of 20 and 50? (Including 20 and 50)
 /*
-* Write your query here
+* SELECT avg (income( FRIOM customers
+WHERE age BETWEEN 20 AND 50;
 */
